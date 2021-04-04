5e39d15eb13b19c207ac5a9b78029578112ddac2
senta2006 <senta2006@naver.com>
2021-04-05 00:13:43 +0900

    user2 first commit

---
+++ SWtest/Project1/Project1/SWtest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "This is test" << endl;
+	cout << "Software Engineering" << endl << endl;
+	
+	cout << "new line";
+
+	cout<<"This is user2";
+	return 0;
+}
